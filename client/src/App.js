4ba4942a6606444527c4a222a875e3fb8ccf1705
Oliver Morrison <olivercharlesmorrison@gmail.com>
2021-03-11 11:32:02 +0000

    fix(app.js): updated all references to 'Events' folder, changing case to match reality

--- client/src/App.js
+++ client/src/App.js
@@ -1,82 +1,93 @@
-import './App.css'
-import React, { useEffect, useState } from 'react'
-import EventsApiService from './services/EventsApiService'
-import UsersApiService from './services/UsersApiService'
-import Spinner from './components/Handling/Spinner'
-import { Route, Switch } from 'react-router-dom'
-import Home from './components/Home'
-import Error from './components/Handling/Error'
-import auth from './utils/auth'
-import NavBar from './components/Navbar'
-import Login from './components/User/Login'
-import Register from './components/User/Register'
-import Logout from './components/User/Logout'
-import Profile from './components/User/Profile'
-import Events from './containers/events/Events'
-import EventDetails from './components/Event/EventDetails'
-import NewEvent from './containers/events/NewEvent'
+import './App.css';
+import React, { useEffect, useState } from 'react';
+import EventsApiService from './services/EventsApiService';
+import UsersApiService from './services/UsersApiService';
+import Spinner from './components/Handling/Spinner';
+import { Route, Switch } from 'react-router-dom';
+import Home from './components/Home';
+import Error from './components/Handling/Error';
+import auth from './utils/auth';
+import NavBar from './components/Navbar';
+import Login from './components/User/Login';
+import Register from './components/User/Register';
+import Logout from './components/User/Logout';
+import Profile from './components/User/Profile';
+import Events from './containers/Events/Events';
+import EventDetails from './components/Event/EventDetails';
+import NewEvent from './containers/Events/NewEvent';
 import Footer from './components/Footer';
-import { motion, AnimatePresence } from 'framer-motion'
+import { motion, AnimatePresence } from 'framer-motion';
 
-function App () {
-  const [status, setStatus] = useState(false)
-  const [events, setEvents] = useState([])
-  const initialState = auth.isAuthenticated()
-  const [isAuthenticated, setIsAuthenticated] = useState(initialState)
-  const [user, setUser] = useState({})
+function App() {
+  const [status, setStatus] = useState(false);
+  const [events, setEvents] = useState([]);
+  const initialState = auth.isAuthenticated();
+  const [isAuthenticated, setIsAuthenticated] = useState(initialState);
+  const [user, setUser] = useState({});
 
   useEffect(() => {
     EventsApiService.getEvents()
-      .then(event => setEvents(event))
-      .then(() => setStatus(true))
-  }, [])
+      .then((event) => setEvents(event))
+      .then(() => setStatus(true));
+  }, []);
 
   const createEvent = (body) => {
-    EventsApiService.createEvent(body)
-      .then(event => setEvents([...events, event]))
-  }
+    EventsApiService.createEvent(body).then((event) =>
+      setEvents([...events, event]),
+    );
+  };
   const signUpDown = (dir, id) => {
     if (dir === 'up') {
       EventsApiService.signUp(id)
-        .then(updated => setEvents((events) => {
-          const index = events.findIndex(event => event._id === updated._id)
-          const copy = [...events]
-          copy.splice(index, 1, updated)
-          return copy
-        }))
+        .then((updated) =>
+          setEvents((events) => {
+            const index = events.findIndex(
+              (event) => event._id === updated._id,
+            );
+            const copy = [...events];
+            copy.splice(index, 1, updated);
+            return copy;
+          }),
+        )
         .then(() => updateUser())
-        .then(console.log(user))
+        .then(console.log(user));
     } else {
       EventsApiService.signDown(id)
-        .then(updated => setEvents((events) => {
-          const index = events.findIndex(event => event._id === updated._id)
-          const copy = [...events]
-          copy.splice(index, 1, updated)
-          return copy
-        }))
+        .then((updated) =>
+          setEvents((events) => {
+            const index = events.findIndex(
+              (event) => event._id === updated._id,
+            );
+            const copy = [...events];
+            copy.splice(index, 1, updated);
+            return copy;
+          }),
+        )
         .then(() => updateUser())
-        .then(console.log(user))
+        .then(console.log(user));
     }
-  }
-  function updateUser () {
-    UsersApiService.profile()
-      .then(updated => setUser(updated))
+  };
+  function updateUser() {
+    UsersApiService.profile().then((updated) => setUser(updated));
   }
   return (
     <div className="App">
-      {
-        !status
-          ? <Spinner />
-          : <main>
-            <NavBar isAuthenticated={isAuthenticated} user={user} />
-            {/* <Image /> */}
-            <AnimatePresence>
+      {!status ? (
+        <Spinner />
+      ) : (
+        <main>
+          <NavBar isAuthenticated={isAuthenticated} user={user} />
+          {/* <Image /> */}
+          <AnimatePresence>
             <Switch>
-              <Route path='/' exact component={Home} />
+              <Route path="/" exact component={Home} />
               <Route
                 path="/register"
                 render={(props) => (
-                  <Register {...props} setIsAuthenticated={setIsAuthenticated} />
+                  <Register
+                    {...props}
+                    setIsAuthenticated={setIsAuthenticated}
+                  />
                 )}
               />
               <Route
@@ -91,29 +102,43 @@ function App () {
                   <Logout {...props} setIsAuthenticated={setIsAuthenticated} />
                 )}
               />
-              <Route path='/new-event' render={() => (
-                <NewEvent createEvent={createEvent} user={user} />
-              )} />
-              <Route path="/profile" render={() => (
-                <Profile user={user} setUser={setUser} />)} />
+              <Route
+                path="/new-event"
+                render={() => (
+                  <NewEvent createEvent={createEvent} user={user} />
+                )}
+              />
+              <Route
+                path="/profile"
+                render={() => <Profile user={user} setUser={setUser} />}
+              />
               {/* <Route path='/my-events' exact render={() => (
                 <MyEvents user={user} />)} /> */}
 
-              <Route path='/events' exact render={() => (
-                <Events value={events}/>
-              )} />
-              <Route path='/events/:id' render={() => (
-                <EventDetails events={events} signUpDown={signUpDown} user={user} />
-              )} />
+              <Route
+                path="/events"
+                exact
+                render={() => <Events value={events} />}
+              />
+              <Route
+                path="/events/:id"
+                render={() => (
+                  <EventDetails
+                    events={events}
+                    signUpDown={signUpDown}
+                    user={user}
+                  />
+                )}
+              />
               {/* <Route path='/users/:id' exact component={UserDetails}/> */}
               <Route component={Error} />
             </Switch>
-            </AnimatePresence>
-            <Footer />
-          </main>
-      }
+          </AnimatePresence>
+          <Footer />
+        </main>
+      )}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;

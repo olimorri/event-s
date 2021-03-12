edec0f2b4024cfbcf36aec21177be9ef66a379af
Oliver Morrison <olivercharlesmorrison@gmail.com>
2021-03-12 18:27:28 +0000

    refactor(login.tsx): refactored to use typescript - all working, includes type guarding

--- client/src/components/User/Login.tsx
+++ client/src/components/User/Login.tsx
@@ -24,7 +24,7 @@ const initialState = {
 };
 
 export default function Login(props: UserProps) {
-  const [state, setState] = useState(initialState); //TODO: where does the intial state value actually come from?
+  const [state, setState] = useState(initialState);
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const name: string = e.target.name;
@@ -40,104 +40,113 @@ export default function Login(props: UserProps) {
     const email: string = state.email;
     const password: string = state.password;
     const user: {} = { email, password };
-    const res: User | UserRequestError = await UsersApiService.login(user);
-    console.log(res);
-    if (res.error) {
-      alert(`${res.message}`);
-      setState(initialState);
-    } else {
+
+    const res = await UsersApiService.login(user);
+
+    function isUser(res: any): res is User {
+      return res.error === undefined;
+    }
+
+    function isUserRequest(res: any): res is UserRequestError {
+      return res.error !== undefined;
+    }
+
+    if (isUser(res)) {
       props.setIsAuthenticated(true);
       auth.login(() => props.history.push('/profile'));
+    } else if (isUserRequest(res)) {
+      alert(`${res.message}`);
+      setState(initialState);
     }
-  };
 
-  const validateForm = (): boolean => {
-    return !state.email || !state.password;
-  };
+    const validateForm = (): boolean => {
+      return !state.email || !state.password;
+    };
 
-  return (
-    <Flex
-      minH={'100vh'}
-      align={'center'}
-      justify={'center'}
-      bg={'custom.100'}
-      bgImage="url('https://res.cloudinary.com/dujun1hoe/image/upload/v1615228154/event-s/gradient-background-26046-26731-hd-wallpapers.jpg_cenrqe.png')"
-      bgSize="100%"
-      backgroundRepeat="no-repeat"
-    >
-      <motion.div
-        initial={{
-          opacity: 0,
-          x: '-100vw',
-        }}
-        animate={{
-          opacity: 1,
-          x: 0,
-        }}
-        transition={{
-          // @ts-ignore
-          type: 'spring',
-          // @ts-ignore
-          stiffness: 100,
-          // @ts-ignore
-          ease: 'easeIn',
-        }}
-        exit={{
-          opacity: 0,
-          x: '-100vw',
-        }}
+    return (
+      <Flex
+        minH={'100vh'}
+        align={'center'}
+        justify={'center'}
+        bg={'custom.100'}
+        bgImage="url('https://res.cloudinary.com/dujun1hoe/image/upload/v1615228154/event-s/gradient-background-26046-26731-hd-wallpapers.jpg_cenrqe.png')"
+        bgSize="100%"
+        backgroundRepeat="no-repeat"
       >
-        <Stack spacing={8} mx={'auto'} maxW={'lg'} py={12} px={6}>
-          <Stack align={'center'}>
-            <Heading color={'white'} fontSize={'4xl'}>
-              Sign in to your account
-            </Heading>
-          </Stack>
-          <Box rounded={'lg'} bg={'white'} boxShadow={'lg'} p={8}>
-            <Stack spacing={4}>
-              <FormControl id="email">
-                <FormLabel>Email address</FormLabel>
-                <Input
-                  type="email"
-                  id="email"
-                  placeholder="name@email.com"
-                  name="email"
-                  value={state.email}
-                  onChange={handleChange}
-                />
-              </FormControl>
-              <Stack spacing={10}>
-                <FormControl id="password">
-                  <FormLabel>Password</FormLabel>
+        <motion.div
+          initial={{
+            opacity: 0,
+            x: '-100vw',
+          }}
+          animate={{
+            opacity: 1,
+            x: 0,
+          }}
+          transition={{
+            // @ts-ignore
+            type: 'spring',
+            // @ts-ignore
+            stiffness: 100,
+            // @ts-ignore
+            ease: 'easeIn',
+          }}
+          exit={{
+            opacity: 0,
+            x: '-100vw',
+          }}
+        >
+          <Stack spacing={8} mx={'auto'} maxW={'lg'} py={12} px={6}>
+            <Stack align={'center'}>
+              <Heading color={'white'} fontSize={'4xl'}>
+                Sign in to your account
+              </Heading>
+            </Stack>
+            <Box rounded={'lg'} bg={'white'} boxShadow={'lg'} p={8}>
+              <Stack spacing={4}>
+                <FormControl id="email">
+                  <FormLabel>Email address</FormLabel>
                   <Input
-                    type="password"
-                    placeholder="Choose a password"
-                    name="password"
-                    value={state.password}
+                    type="email"
+                    id="email"
+                    placeholder="name@email.com"
+                    name="email"
+                    value={state.email}
                     onChange={handleChange}
                   />
                 </FormControl>
-                <Button
-                  bg={'custom.200'}
-                  color={'white'}
-                  _hover={{
-                    bg: 'custom.300',
-                  }}
-                  isDisabled={validateForm()}
-                  onClick={handleClick}
-                >
-                  Sign in
-                </Button>
+                <Stack spacing={10}>
+                  <FormControl id="password">
+                    <FormLabel>Password</FormLabel>
+                    <Input
+                      type="password"
+                      placeholder="Choose a password"
+                      name="password"
+                      value={state.password}
+                      onChange={handleChange}
+                    />
+                  </FormControl>
+                  <Button
+                    bg={'custom.200'}
+                    color={'white'}
+                    _hover={{
+                      bg: 'custom.300',
+                    }}
+                    isDisabled={validateForm()}
+                    onClick={handleClick}
+                  >
+                    Sign in
+                  </Button>
+                </Stack>
+                <RouterLink to="/register">
+                  <Text align={'end'} color="blue.400">
+                    Create new account
+                  </Text>
+                </RouterLink>
               </Stack>
-              <RouterLink to="/register">
-                <Text align={'end'} color="blue.400">
-                  Create new account
-                </Text>
-              </RouterLink>
-            </Stack>
-          </Box>
-        </Stack>
-      </motion.div>
-    </Flex>
-  );
+            </Box>
+          </Stack>
+        </motion.div>
+      </Flex>
+    );
+  };
 }
--- client/src/interfaces/Event.ts
+++ client/src/interfaces/Event.ts
@@ -1,15 +1,15 @@
 export interface Event {
-  attendees: number
-date: string
-description: string
-duration: number
-geolocation: string
-limit: number
-location: string
-name: string
-owner: string
-photo: string
-type: string
-__v?: number
-_id: string
+  attendees: number;
+  date: string;
+  description: string;
+  duration: number;
+  geolocation: string;
+  limit: number;
+  location: string;
+  name: string;
+  owner: string;
+  photo: string;
+  type: string;
+  __v?: number;
+  _id: string;
 }

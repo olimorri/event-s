6caee3f3fb6c73fb6410ca9b79ccef19a07ac944
Oliver Morrison <olivercharlesmorrison@gmail.com>
2021-03-12 15:45:35 +0000

    feat: created User interface, __v and _id are optional

---
+++ client/src/interfaces/User.ts
@@ -0,0 +1,11 @@
+export interface User {
+  email: string;
+  eventList: string[];
+  firstName: string;
+  lastName: string;
+  host: boolean;
+  password: string;
+  photo: string;
+  __v?: number;
+  _id?: string;
+}

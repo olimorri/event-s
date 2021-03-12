be40039a884bcec54d874be494736d97120fd8ea
Oliver Morrison <olivercharlesmorrison@gmail.com>
2021-03-12 18:32:01 +0000

    fix: sorted bug where handleClick encompassed whole file

--- client/src/components/User/Login.tsx
+++ client/src/components/User/Login.tsx
@@ -58,95 +58,95 @@ export default function Login(props: UserProps) {
       alert(`${res.message}`);
       setState(initialState);
     }
+  };
 
-    const validateForm = (): boolean => {
-      return !state.email || !state.password;
-    };
+  const validateForm = (): boolean => {
+    return !state.email || !state.password;
+  };
 
-    return (
-      <Flex
-        minH={'100vh'}
-        align={'center'}
-        justify={'center'}
-        bg={'custom.100'}
-        bgImage="url('https://res.cloudinary.com/dujun1hoe/image/upload/v1615228154/event-s/gradient-background-26046-26731-hd-wallpapers.jpg_cenrqe.png')"
-        bgSize="100%"
-        backgroundRepeat="no-repeat"
+  return (
+    <Flex
+      minH={'100vh'}
+      align={'center'}
+      justify={'center'}
+      bg={'custom.100'}
+      bgImage="url('https://res.cloudinary.com/dujun1hoe/image/upload/v1615228154/event-s/gradient-background-26046-26731-hd-wallpapers.jpg_cenrqe.png')"
+      bgSize="100%"
+      backgroundRepeat="no-repeat"
+    >
+      <motion.div
+        initial={{
+          opacity: 0,
+          x: '-100vw',
+        }}
+        animate={{
+          opacity: 1,
+          x: 0,
+        }}
+        transition={{
+          // @ts-ignore
+          type: 'spring',
+          // @ts-ignore
+          stiffness: 100,
+          // @ts-ignore
+          ease: 'easeIn',
+        }}
+        exit={{
+          opacity: 0,
+          x: '-100vw',
+        }}
       >
-        <motion.div
-          initial={{
-            opacity: 0,
-            x: '-100vw',
-          }}
-          animate={{
-            opacity: 1,
-            x: 0,
-          }}
-          transition={{
-            // @ts-ignore
-            type: 'spring',
-            // @ts-ignore
-            stiffness: 100,
-            // @ts-ignore
-            ease: 'easeIn',
-          }}
-          exit={{
-            opacity: 0,
-            x: '-100vw',
-          }}
-        >
-          <Stack spacing={8} mx={'auto'} maxW={'lg'} py={12} px={6}>
-            <Stack align={'center'}>
-              <Heading color={'white'} fontSize={'4xl'}>
-                Sign in to your account
-              </Heading>
-            </Stack>
-            <Box rounded={'lg'} bg={'white'} boxShadow={'lg'} p={8}>
-              <Stack spacing={4}>
-                <FormControl id="email">
-                  <FormLabel>Email address</FormLabel>
+        <Stack spacing={8} mx={'auto'} maxW={'lg'} py={12} px={6}>
+          <Stack align={'center'}>
+            <Heading color={'white'} fontSize={'4xl'}>
+              Sign in to your account
+            </Heading>
+          </Stack>
+          <Box rounded={'lg'} bg={'white'} boxShadow={'lg'} p={8}>
+            <Stack spacing={4}>
+              <FormControl id="email">
+                <FormLabel>Email address</FormLabel>
+                <Input
+                  type="email"
+                  id="email"
+                  placeholder="name@email.com"
+                  name="email"
+                  value={state.email}
+                  onChange={handleChange}
+                />
+              </FormControl>
+              <Stack spacing={10}>
+                <FormControl id="password">
+                  <FormLabel>Password</FormLabel>
                   <Input
-                    type="email"
-                    id="email"
-                    placeholder="name@email.com"
-                    name="email"
-                    value={state.email}
+                    type="password"
+                    placeholder="Choose a password"
+                    name="password"
+                    value={state.password}
                     onChange={handleChange}
                   />
                 </FormControl>
-                <Stack spacing={10}>
-                  <FormControl id="password">
-                    <FormLabel>Password</FormLabel>
-                    <Input
-                      type="password"
-                      placeholder="Choose a password"
-                      name="password"
-                      value={state.password}
-                      onChange={handleChange}
-                    />
-                  </FormControl>
-                  <Button
-                    bg={'custom.200'}
-                    color={'white'}
-                    _hover={{
-                      bg: 'custom.300',
-                    }}
-                    isDisabled={validateForm()}
-                    onClick={handleClick}
-                  >
-                    Sign in
-                  </Button>
-                </Stack>
-                <RouterLink to="/register">
-                  <Text align={'end'} color="blue.400">
-                    Create new account
-                  </Text>
-                </RouterLink>
+                <Button
+                  bg={'custom.200'}
+                  color={'white'}
+                  _hover={{
+                    bg: 'custom.300',
+                  }}
+                  isDisabled={validateForm()}
+                  onClick={handleClick}
+                >
+                  Sign in
+                </Button>
               </Stack>
-            </Box>
-          </Stack>
-        </motion.div>
-      </Flex>
-    );
-  };
+              <RouterLink to="/register">
+                <Text align={'end'} color="blue.400">
+                  Create new account
+                </Text>
+              </RouterLink>
+            </Stack>
+          </Box>
+        </Stack>
+      </motion.div>
+    </Flex>
+  );
 }

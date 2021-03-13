16bf5a9b53a9275993bcce1418b37b9464edaef9
Colin Bellamy <colin.j.bellamy@gmail.com>
2021-03-13 15:19:02 +0000

    refactor(index.js): refactor to TS

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -3,21 +3,21 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+import { ChakraProvider, extendTheme } from '@chakra-ui/react';
 import { BrowserRouter } from 'react-router-dom';
 
 const theme = extendTheme({
   colors: {
     custom: {
-      100: "#EEEDE7",
-      200: "#868B8E",
-      300: "#B9B7BD",
-      400: "#E7D2CC",
-      600: "#0A0708",
-      700: "#747474"
-    }
+      100: '#EEEDE7',
+      200: '#868B8E',
+      300: '#B9B7BD',
+      400: '#E7D2CC',
+      600: '#0A0708',
+      700: '#747474',
+    },
   },
-})
+});
 
 ReactDOM.render(
   <React.StrictMode>
@@ -27,7 +27,7 @@ ReactDOM.render(
       </BrowserRouter>
     </ChakraProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
 
 // If you want to start measuring performance in your app, pass a function

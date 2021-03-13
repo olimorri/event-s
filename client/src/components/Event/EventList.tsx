d15c2ae2d802820c6109cff9c75ccd4e94fc965d
Colin Bellamy <colin.j.bellamy@gmail.com>
2021-03-13 10:33:47 +0000

    refactor(eventlist): update value property name to events

--- client/src/components/Event/EventList.tsx
+++ client/src/components/Event/EventList.tsx
@@ -1,16 +1,15 @@
 import React from 'react';
 import EventCard from './EventCard';
 import { Wrap } from '@chakra-ui/react';
-import { EventListProps } from '../../interfaces/EventListProps';
 import { Event } from '../../interfaces/Event';
 
-export default function EventList({ value }: EventListProps) {
+export default function EventList({ events }: { events: Event[] }) {
   return (
     <Wrap w={'100%'} marginTop={5} align="start">
-      {value
+      {events
         .filter((event: Event) => event.date > new Date().toISOString())
         .map((event: Event) => (
-          <EventCard value={event} key={event._id} />
+          <EventCard event={event} key={event._id} />
         ))}
     </Wrap>
   );

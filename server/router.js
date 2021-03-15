e0d33f33e7bd5337fde6e98928649559ff1195c5
Oliver Morrison <olivercharlesmorrison@gmail.com>
2021-03-15 08:23:45 +0000

    refactor(router.js): removed commented out code from router

--- server/router.js
+++ server/router.js
@@ -3,7 +3,6 @@ const Events = require('./controllers/events');
 const Users = require('./controllers/users');
 const authMiddleware = require('./middlewares/auth');
 
-
 router.get('/events', Events.getEvents);
 router.get('/events/:id', Events.getSingleEvent);
 router.post('/events', authMiddleware, Events.postEvent);
@@ -11,7 +10,6 @@ router.delete('/events/:id', Events.deleteEvent);
 router.put('/events/:id', Events.updateEvent);
 router.post('/events/:id/up', authMiddleware, Events.attendEvent);
 router.post('/events/:id/down', authMiddleware, Events.unattendEvent);
-
 
 router.post('/register', Users.create);
 router.post('/login', Users.login);
@@ -22,44 +20,5 @@ router.post('/logout', authMiddleware, Users.logout);
 router.get('/users', Users.getUsers);
 router.get('/users/:id', Users.getHostDetails);
 router.delete('/users/:id', Users.deleteUser);
-
-
-
-
-// // Photo test
-
-// const fileUploader = require('./cloudinary.config');
-// const UploadedFile = require('./models/uploadedFile.model');
-
-// router.post('/upload', fileUploader.single('file'), (req, res, next) => {
-
-//   if (!req.file) {
-//     next(new Error('No file uploaded!'));
-//     return;
-//   }
-//   const newImage  = new UploadedFile({title: req.file.filename, fileUrl: req.file.path});
-//   newImage.save((err) => {
-//     if (err) {
-
-//       return res.status(500);
-//     }
-//     res.json({ secure_url: req.file.path });
-//   });
-//   res.send(newImage);
-// });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 module.exports = router;

ec92cd8b258282811bc3aee5c8c4ba57f9327d1f
Colin Bellamy <colin.j.bellamy@gmail.com>
2021-03-15 18:39:27 +0000

    refactor: none

--- server/models/index.js
+++ server/models/index.js
@@ -1,16 +1,19 @@
 const mongoose = require('mongoose');
 const { dbName, host, dbPort } = require('../config');
 
-module.exports = mongoose.connect(`mongodb://${host}:${dbPort}/${dbName}`, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useFindAndModify: false,
-},
+module.exports = mongoose.connect(
+  `mongodb://${host}:${dbPort}/${dbName}`,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+  },
 
-(err) => {
-  if (err) {
-    console.log(`😞 Sorry, something went wrong! ${err}`); // eslint-disable-line no-console
-  } else {
-    console.log(`🦆 Database (sessions) connected @ port ${dbPort}!`); // eslint-disable-line no-console
-  }
-});
+  (err) => {
+    if (err) {
+      console.log(`😞 Sorry, something went wrong! ${err}`); // eslint-disable-line no-console
+    } else {
+      console.log(`🦆 Database (sessions) connected @ port ${dbPort}!`); // eslint-disable-line no-console
+    }
+  },
+);

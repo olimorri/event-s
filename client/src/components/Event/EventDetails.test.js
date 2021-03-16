3e158bb1569d2e3c26018253f79c05b52ea22d27
Oliver Morrison <olivercharlesmorrison@gmail.com>
2021-03-16 16:39:29 +0000

    test(eventdetails eventmocks): created mocks and started unit testing on eventDetails

---
+++ client/src/components/Event/EventDetails.test.js
@@ -0,0 +1,47 @@
+import { render, cleanup, screen, waitFor } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
+import EventDetails from './EventDetails';
+import { events, event } from './EventMocks';
+
+afterEach(cleanup);
+
+const EventDetailsTest = () => (
+  <BrowserRouter>
+    <EventDetails events={events} event={event} />
+  </BrowserRouter>
+);
+
+afterEach(cleanup);
+
+describe('Snapshot', () => {
+  it('should take a snapshot', () => {
+    const { asFragment } = render(<EventDetailsTest />);
+    expect(asFragment(<EventDetailsTest />)).toMatchSnapshot();
+  });
+});
+
+describe('Unit Testing', () => {
+  // test('renders img', () => {
+  //   render(<EventDetailsTest />);
+  //   const displayedImg = document.querySelector('img');
+  //   expect(displayedImg).toBeValid();
+  // });
+
+  test('rendered event name matches props event.name', async () => {
+    render(<EventDetailsTest />);
+    const loaded = await waitFor(() => expect(event.name).toBeInTheDocument());
+    //const eventName = screen.getByText(event.name);
+  });
+
+  // test('rendered event postcode matches props event.location', () => {
+  //   render(<EventDetailsTest />);
+  //   const eventPostcode = screen.getByText(event.location);
+  //   expect(eventPostcode).toBeInTheDocument();
+  // });
+
+  // test('rendered event date matches props event.date', () => {
+  //   render(<EventDetailsTest />);
+  //   const eventDate = screen.getByText(/Mar 23rd 21/i);
+  //   expect(eventDate).toBeInTheDocument();
+  // });
+});

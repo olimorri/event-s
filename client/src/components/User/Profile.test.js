2d8c6a1ebbf845fdd392993d6fdee96d38e50f2d
Colin Bellamy <colin.j.bellamy@gmail.com>
2021-03-16 16:57:22 +0000

    test(eventmocks): edit eventmocks

--- client/src/components/User/Profile.test.js
+++ client/src/components/User/Profile.test.js
@@ -1,46 +1,14 @@
 import { render, cleanup, screen, fireEvent } from '@testing-library/react';
-import Profile from './Profile';
-import EventDetails from '../Event/EventDetails';
 import { BrowserRouter, Router } from 'react-router-dom';
 import { createMemoryHistory } from 'history';
+import Profile from './Profile';
+import { events, event } from '../Event/EventMocks';
 
 afterEach(cleanup);
 
-const event1 = {
-  attendees: 1,
-  date: '2021-03-23T00:00:00.000Z',
-  description: 'This is a test event',
-  duration: 1,
-  geolocation: '0.261773,51.116312',
-  limit: 1,
-  list: ['604a36891b95d669d99ebe2e'],
-  location: 'tn25hs',
-  name: 'test event 1',
-  owner: '604b5c00ca29f24f751f492c',
-  photo: 'https://source.unsplash.com/random/300x300',
-  type: 'Food',
-  _id: '604b5c00ca29f24f751f492c',
-};
-
-const event2 = {
-  attendees: 2,
-  date: '2021-03-24T00:00:00.000Z',
-  description: 'This is also a test event',
-  duration: 2,
-  geolocation: '0.261773,51.116312',
-  limit: 2,
-  list: ['604a36891b95d669d99ebe2f'],
-  location: 'tn25hd',
-  name: 'test event 2',
-  owner: '604b5c00ca29f24f751f492c',
-  photo: 'https://source.unsplash.com/random/300x300',
-  type: 'Other',
-  _id: '604b5c00ca29f24f751f492c',
-};
-
 const user = {
   email: 'test@email.com',
-  eventList: [event1, event2],
+  eventList: events,
   firstName: 'Bob',
   lastName: 'Bobson',
   host: true,
@@ -96,8 +64,8 @@ describe('Unit Testing', () => {
         <Profile user={user} />
       </BrowserRouter>,
     );
-    const event1 = screen.getByText(/^test event 1$/i);
-    const event2 = screen.getByText(/^test event 2$/i);
+    const event1 = screen.getByText(/^Test Event 1$/i);
+    const event2 = screen.getByText(/^Test Event 2$/i);
     expect(event1).toBeInTheDocument();
     expect(event2).toBeInTheDocument();
   });
@@ -108,7 +76,7 @@ describe('Unit Testing', () => {
         <Profile user={user} />
       </BrowserRouter>,
     );
-    const numberOfEvents = screen.getByText(/^2$/i);
+    const numberOfEvents = screen.getByText(/^8$/i);
     expect(numberOfEvents).toBeInTheDocument();
   });
 
@@ -119,18 +87,18 @@ describe('Unit Testing', () => {
         <Profile user={user} />
       </Router>,
     );
-    const myEvent = screen.getByText(/^test event 1$/i);
+    const myEvent = screen.getByText(/^Test Event 1$/i);
     expect(history.location).toHaveProperty('pathname', '/');
     expect(history.entries).toHaveLength(1);
     fireEvent.click(myEvent);
     expect(history.location).toHaveProperty(
       'pathname',
-      '/events/604b5c00ca29f24f751f492c',
+      '/events/604a268300da7a8522af8b23',
     );
     expect(history.entries).toHaveLength(2);
     expect(history.entries[1]).toHaveProperty(
       'pathname',
-      '/events/604b5c00ca29f24f751f492c',
+      '/events/604a268300da7a8522af8b23',
     );
   });
 });
